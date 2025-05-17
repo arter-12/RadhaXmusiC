28548dd6ac97ac96262c2ab4f42a17c8adf75c3f
arter-12 <telegramotp000@gmail.com>
2025-05-17 14:24:52 +0530

    Update __init__.py

--- AnonXMusic/__init__.py
+++ AnonXMusic/__init__.py
@@ -1,8 +1,8 @@
-from RadhaXmusiC.core.bot import Anony
-from RadhaXmusiC.core.dir import dirr
-from RadhaXmusiC.core.git import git
-from RadhaXmusiC.core.userbot import Userbot
-from RadhaXmusiC.misc import dbb, heroku
+from AnonXMusic.core.bot import Anony
+from AnonXMusic.core.dir import dirr
+from AnonXMusic.core.git import git
+from AnonXMusic.core.userbot import Userbot
+from AnonXMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
